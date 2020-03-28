df040c2afdb2162a0b6e927af8e46485cbe44380
Amber Williams <awilliams1234@live.com>
2020-03-28 20:47:25 +0000

    Setting up attr_accessors and initialize method

--- lib/project_cli/beauty.rb
+++ lib/project_cli/beauty.rb
@@ -1,3 +1,24 @@
 class Beauty
   
+  attr_accessor :brand, :name, :price, :description, :product_type
+  
+  @@all = []
+  
+  def initialize(:brand, :name, :price, :description, :product_type)
+    @brand = brand
+    @name = name
+    @price = price
+    @description = description
+    @product_type = product_type
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def 
 end
