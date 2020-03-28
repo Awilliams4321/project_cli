a4ae16e816155d7c04937db75547726e2561f57e
Amber Williams <awilliams1234@live.com>
2020-03-28 23:38:30 +0000

    Iterating over the array returned from the API to get a key and value of each character.

--- lib/project_cli/api.rb
+++ lib/project_cli/api.rb
@@ -1,7 +1,11 @@
 class API
-
+  
   def self.get_characters
     response = HTTParty.get("https://rickandmortyapi.com/api/character/")
-  end
-
-end
+    characters = ['results']
+    characters.each do |k, v|
+      
+      binding.pry 
+    end 
+  end 
+end 
