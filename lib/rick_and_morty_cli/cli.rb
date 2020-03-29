a774847650d8d9a1c89aa1f8525ca19275ac7b8e
Amber Williams <awilliams1234@live.com>
2020-03-29 21:01:46 +0000

    Making adjustments to the user greeting.

--- lib/rick_and_morty_cli/cli.rb
+++ lib/rick_and_morty_cli/cli.rb
@@ -9,14 +9,16 @@ class CLI
   def self.welcome
     puts "Welcome, fan!"
     sleep (2)
-    puts "To see more about the character you are interested in, enter the characters name."
+    puts "I'm PICKLE RIIIICK!"
+    puts "To see more info about other characters on the show, enter their name."
     sleep(2)
     puts "To exit, enter 'exit'."
   end 
   
   def self.goodbye
-    puts "See you later..."
+    puts "See you later! And next time..."
     sleep(2)
+    puts "STAY OUT OF MY PERSONAL SPACE!"
   end
   
   def self.get_char_list 
