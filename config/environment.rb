c0d233e49bfa417feb5e3f25f911aae412453a23
Amber Williams <awilliams1234@live.com>
2020-03-29 20:24:35 +0000

    Changing my require_relative's from project_cli to rick_and_morty_cli because i have changed the file names.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 require 'pry'
 
-require_relative "../lib/project_cli/api"
-require_relative "../lib/project_cli/characters"
-require_relative "../lib/project_cli/cli"
-require_relative "../lib/project_cli/version"
+require_relative "../lib/rick_and_morty_cli/api"
+require_relative "../lib/rick_and_morty_cli/characters"
+require_relative "../lib/rick_and_morty_cli/cli"
+require_relative "../lib/rick_and_morty_cli/version"
