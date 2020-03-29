2ffa538f73c868e4f5ca64b58dbf676c43b655b0
Amber Williams <awilliams1234@live.com>
2020-03-29 03:12:25 +0000

    Using mass assignment to allow my initialize method to iterate over an infinite amount of hashes and assign each key to its value.

--- lib/project_cli/characters.rb
+++ lib/project_cli/characters.rb
@@ -1,23 +1,29 @@
 class Characters
+  @@all = []
   
   attr_accessor :name, :status, :species, :gender, :origin
   
-  @@all = []
+  # def initialize(name, status, species, gender, origin)
+  #   @name = name
+  #   @status = status
+  #   @species = species
+  #   @gender = gender
+  #   @origin = origin
+  # end 
   
-  def initialize(name, status, species, gender, origin)
-    @name = name
-    @status = status
-    @species = species
-    @gender = gender
-    @origin = origin
-  end 
-  
-  def self.all
-    @@all
+  def initialize(hash)
+    hash.each do |k, v|
+      self.send(("#{k}="), v) 
+    end 
+    save
   end 
   
   def save 
     @@all << self
+  end 
+  
+  def self.all
+    @@all
   end 
   
 end
